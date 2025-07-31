ad03f599469246495be4485df1396cd7b6575df6
Tim <timdesmet00@gmail.com>
2025-07-31 18:12:57 +0200

    mise en couleur

--- ConsoleApp1/GenerateurEnnemi.cs
+++ ConsoleApp1/GenerateurEnnemi.cs
@@ -64,50 +64,66 @@ namespace ConsoleApp1
 
         public static void EntrerRatGeant()
         {
+            Console.ForegroundColor = ConsoleColor.DarkGray;
             Console.WriteLine("\nUn grondement sourd résonne sous tes pieds…");
             Console.WriteLine("Un rat géant surgit, son pelage souillé et ses crocs dégoulinants, prêt à te dévorer !\n");
+            Console.ResetColor();
         }
 
         public static void EntrerGobelin()
         {
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("\nUn rire nasillard perce l’air stagnant de l’arène…");
             Console.WriteLine("Un gobelin bondit hors d’une trappe, brandissant une dague aussi tordue que son sourire.\n");
+            Console.ResetColor();
         }
 
         public static void EntrerHobgobelin()
         {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
             Console.WriteLine("\nDes pas lourds ébranlent le sol…");
             Console.WriteLine("Le hobgobelin entre, cuir clouté et regard de brute, ses muscles tendus d’impatience.\n");
+            Console.ResetColor();
         }
 
         public static void EntrerOrc()
         {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
             Console.WriteLine("\nUn hurlement bestial fend le silence…");
             Console.WriteLine("L’orc s’élance, hache levée, couvert de cicatrices et d’un sang qui n’est pas le sien.\n");
+            Console.ResetColor();
         }
 
         public static void EntrerHommeLezard()
         {
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
             Console.WriteLine("\nUn souffle sifflant glisse entre les colonnes de pierre…");
             Console.WriteLine("L’homme-lézard se dresse, langue bifide fouettant l’air, prêt à fondre sur sa proie.\n");
+            Console.ResetColor();
         }
 
         public static void EntrerVelra()
         {
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("\nUn murmure malsain envahit l’arène, glacé comme la mort…");
             Console.WriteLine("Velra l’Écorcheuse apparaît, sa lame traînant sur le sol, laissant derrière elle une traînée de cendre.\n");
+            Console.ResetColor();
         }
 
         public static void EntrerNox()
         {
+            Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine("\nUn choc métallique résonne violemment, puis le silence…");
             Console.WriteLine("Nox le Sanguinaire entre dans la lumière, son armure tachée, son regard vide de toute pitié.\n");
+            Console.ResetColor();
         }
 
         public static void EntrerKira()
         {
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("\nDes chaînes grincent lentement, presque comme un sanglot…");
             Console.WriteLine("Kira la Maudite s’avance lentement, ses yeux noyés dans les ombres du passé.\n");
+            Console.ResetColor();
         }
     }
 }
